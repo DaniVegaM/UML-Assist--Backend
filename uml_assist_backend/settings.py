0723d76886d43046022cbeb58cf2a341e2376365
danivegam <dany.17.com@hotmail.com>
2026-03-07 17:11:15 -0600

    fix: serving media from django

--- uml_assist_backend/settings.py
+++ uml_assist_backend/settings.py
@@ -197,7 +197,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Media files (imagenes subidas por usuarios)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 
 
 #Frontend y token de restablecimiento
